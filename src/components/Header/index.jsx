45ec18c44d0600138be18651ab0c505a3624e693
DashkevichAS <antonsnov0604@gmail.com>
2022-12-28 16:09:24 +0300

    add routing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   const isAuth = false;
@@ -11,29 +12,33 @@ export const Header = () => {
 
   return (
     <div className={styles.root}>
-      <Container maxWidth="lg">
+      <Container maxWidth='lg'>
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to='/'>
             <div>ARCHAKOV BLOG</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
-                  <Button variant="contained">Написать статью</Button>
-                </a>
-                <Button onClick={onClickLogout} variant="contained" color="error">
+                <Link to='/add-post'>
+                  <Button variant='contained'>Написать статью</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant='contained'
+                  color='error'
+                >
                   Выйти
                 </Button>
               </>
             ) : (
               <>
-                <a href="/login">
-                  <Button variant="outlined">Войти</Button>
-                </a>
-                <a href="/register">
-                  <Button variant="contained">Создать аккаунт</Button>
-                </a>
+                <Link to='/login'>
+                  <Button variant='outlined'>Войти</Button>
+                </Link>
+                <Link to='/register'>
+                  <Button variant='contained'>Создать аккаунт</Button>
+                </Link>
               </>
             )}
           </div>
